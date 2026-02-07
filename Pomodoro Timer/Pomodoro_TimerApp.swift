345e6d34e55837e7f8b0dc66b8c811048f182d56
Austen & McKenzie <phil@austenmckenzie.com>
2026-02-07 09:59:12 +0800

    Initial Commit

---
+++ Pomodoro Timer/Pomodoro_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pomodoro_TimerApp.swift
+//  Pomodoro Timer
+//
+//  Created by Phil McKenzie on 7/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct Pomodoro_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
